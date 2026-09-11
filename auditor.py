2ef9c0fd0bc39a1ed382a4c77d2e57384fb70e7b
Ruben <loruben123@gmail.com>
2026-09-11 15:21:17 +0800

    Scenario 1 to 8 is done

--- auditor.py
+++ auditor.py
@@ -1,13 +1,15 @@
 inventory = 0
+failed_inputs = 0
+isrunning = True
 
-
-def call_programme(isrunning, inventory):
-    isrunning = True
+def call_programme(isrunning, inventory, failed_inputs):
+    
     while isrunning:
         print("Current inventory is: {}".format(inventory) )
         userinput = input("Please input your inventory count: " )
         if userinput.lower() == "quit":
-            print("ending")
+            print("Total units processed: {}".format(inventory))
+            print ("Failed/Rejected entries: {}".format(failed_inputs))
             break
         elif userinput.isdigit():
             userint = int(userinput)
@@ -20,7 +22,8 @@ def call_programme(isrunning, inventory):
         else:
             isrunning = False
             print("ERROR, PLEASE INPUT IN A POSITIVE NUMBER")
-        call_programme(True,inventory)
+            failed_inputs += 1
+        call_programme(True,inventory, failed_inputs)
 
 
-call_programme(True, inventory)
+call_programme(True, inventory, failed_inputs)
