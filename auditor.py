2be2191398f63febe73117fae52a20fc620f66e2
Ruben <loruben123@gmail.com>
2026-09-11 15:16:40 +0800

    Fulfilled scenarios 1 to 6

--- auditor.py
+++ auditor.py
@@ -1,10 +1,26 @@
-
-
 inventory = 0
 
-userinput = input("Please input your inventory count: " )
 
-if userinput.isdigit():
-    print("its a digit")
-else:
-    print("error")
+def call_programme(isrunning, inventory):
+    isrunning = True
+    while isrunning:
+        print("Current inventory is: {}".format(inventory) )
+        userinput = input("Please input your inventory count: " )
+        if userinput.lower() == "quit":
+            print("ending")
+            break
+        elif userinput.isdigit():
+            userint = int(userinput)
+            if userint >= 0:
+                isrunning = False
+                inventory = inventory + int(userinput)
+            if userint >= 500:
+                print("ALERT, STORAGE IS FULL")
+                break
+        else:
+            isrunning = False
+            print("ERROR, PLEASE INPUT IN A POSITIVE NUMBER")
+        call_programme(True,inventory)
+
+
+call_programme(True, inventory)
